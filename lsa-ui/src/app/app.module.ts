917650c83d859b22399bdd14af23d62824dfdf7a
neelesh.pandey <neelesh.pandey@infosys.com>
2020-03-13 20:51:28 +0530

    removed unnecessary import from appmodule

--- lsa-ui/src/app/app.module.ts
+++ lsa-ui/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AppMaterialModule } from './app-material.module';
+import { AppRoutingModule } from '@/app-routing.module';
+import { AppComponent } from '@/app.component';
+
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -19,7 +19,6 @@ import { SharedModule } from '@/_compenent/shared.module';
     HomeModule,
     SharedModule,
     AppRoutingModule,
-    AppMaterialModule,
     FlexLayoutModule,
     BrowserAnimationsModule
   ],
