9bd48c8ca0e438f7a3e67d6f317c75c793c1ca1d
neelesh.pandey <neelesh.pandey@infosys.com>
2020-03-13 20:52:09 +0530

    imported app material module in shared module to use in header

--- lsa-ui/src/app/_compenent/shared.module.ts
+++ lsa-ui/src/app/_compenent/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '@/_compenent/header/header.component';
-
+import { AppMaterialModule } from '@/app-material.module';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppMaterialModule
   ],
   exports: [HeaderComponent]
 })
