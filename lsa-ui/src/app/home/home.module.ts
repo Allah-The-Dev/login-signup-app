689b0f52c24fad73742005fd1d89be2531f2b9e7
neelesh.pandey <neelesh.pandey@infosys.com>
2020-03-16 10:42:57 +0530

    removed auth card from header to welcome

--- lsa-ui/src/app/home/home.module.ts
+++ lsa-ui/src/app/home/home.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AppMaterialModule } from '@/app-material.module';
 
 
 
 @NgModule({
   declarations: [WelcomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppMaterialModule
   ],
   exports: [WelcomeComponent]
 })
