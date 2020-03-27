a18572df1ebb4f6eff60b07e572586987cf89887
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-03-27 13:50:48 +0530

    addid page redirection in home page

--- lsa-ui/src/app/home/home.module.ts
+++ lsa-ui/src/app/home/home.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppMaterialModule } from '@/app-material.module';
-
+import { AppRoutingModule } from '@/app-routing.module';
 
 
 @NgModule({
   declarations: [WelcomeComponent],
   imports: [
     CommonModule,
-    AppMaterialModule
+    AppMaterialModule,
+    AppRoutingModule
   ],
   exports: [WelcomeComponent]
 })
