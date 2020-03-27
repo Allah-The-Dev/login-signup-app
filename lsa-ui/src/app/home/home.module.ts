16831fd7cd4ceb443a5ef38e540a0dcd74ed9126
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-03-27 13:56:27 +0530

    changed import path for welcome component

--- lsa-ui/src/app/home/home.module.ts
+++ lsa-ui/src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent } from '@/home/welcome/welcome.component';
 import { AppMaterialModule } from '@/app-material.module';
 import { AppRoutingModule } from '@/app-routing.module';
 
