b3c6eb96092b03590eeed9e89f62d1afe78c2c3c
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-03-31 20:10:57 +0530

    adding material form in signup page

--- lsa-ui/src/app/auth/auth.module.ts
+++ lsa-ui/src/app/auth/auth.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from '@/auth/auth-routing.module';
 import { LoginComponent } from '@/auth/login/login.component';
 import { SignupComponent } from '@/auth/signup/signup.component';
+import { AppMaterialModule } from '@/app-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    AppMaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- lsa-ui/src/app/auth/signup/signup.component.ts
+++ lsa-ui/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm = this.fb.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    contactNo: ['', Validators.required],
+    dob: ['', Validators.required],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  get signupFormControls() {
+    return this.signupForm.controls;
+  }
+
+  submitSignupForm() {
+    console.log(this.signupForm.value)
   }
 
 }
